4274aae6e786b130a8a7fc9e9d33cacac62a8a04
kiprotichkailel <kailelbots22938@gmail.com>
2024-12-05 17:02:27 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,4 @@
-// Contact Form Submission
 document.getElementById('contactForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-  
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-  
-    if (name && email && message) {
-      alert(`Thank you, ${name}! Your message has been sent.`);
-      document.getElementById('contactForm').reset();
-    } else {
-      alert('Please fill out all fields before submitting.');
-    }
-  });
-  
+  e.preventDefault();
+  alert('Thank you for your message. I will get back to you soon!');
+});
